90c4c9f5b582bd7884426cf269970f64f57570c3
x00dd <161578001+x00dd@users.noreply.github.com>
2024-06-05 13:34:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,3 +25,18 @@ resource "aws_ec2_host" "test" {
   host_recovery     = "on"
   auto_placement    = "on"
 }
+
+# Create an AMI that will start a machine whose root device is backed by
+# an EBS volume populated from a snapshot. We assume that such a snapshot
+# already exists with the id "snap-xxxxxxxx".
+resource "aws_ami" "example" {
+  name                = "ami-0ff8a91507f77f867"
+  virtualization_type = "hvm"
+  root_device_name    = "/dev/xvda"
+  imds_support        = "v2.0" # Enforce usage of IMDSv2. You can safely remove this line if your application explicitly doesn't support it.
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    snapshot_id = "snap-xxxxxxxx"
+    volume_size = 8
+  }
+}
